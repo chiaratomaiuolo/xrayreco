ae17e66d6c3d1899227a486f5c37bee1308ea5a3
chiara <chiara.it98@gmail.com>
2024-06-13 19:28:40 +0200

    added lodaing weights mode

--- xrayreco/nnmodels.py
+++ xrayreco/nnmodels.py
@@ -10,16 +10,19 @@ from keras.models import Model, Sequential
 from matplotlib import pyplot as plt
 
 
-def DNN_e(input_shape: Tuple=(7,3)) -> Model:
+def DNN_e(input_shape: Tuple=(7,3), path_to_weights: str=None) -> Model:
     '''Definition of the Deep Neural Network model for energy reconstruction.
     This function defines the chosen architecture for the NN and returns the
     (untrained) keras.Model.
     Arguments
     ---------
-    - input_shape : Tuple
+    input_shape : Tuple
         Shape of the input in the for of a Tuple, that is the format taken as
         input by keras.Input(). The default value is set to (7,3), that is the
         shape of a photon track of 7 pixels, having for each pixel [pha, x, y].
+    path_to_weights : str
+        Path to a .weights.h5 file to be loaded on the model (after having checked
+        its compatibility with the NN architecture).
     '''
     model_e = Sequential([
                     Input(shape=input_shape),
@@ -37,19 +40,32 @@ def DNN_e(input_shape: Tuple=(7,3)) -> Model:
                     # Missing a Dropout layer, ask Rizzi []
                     Dense(1, activation='linear')
                     ])
+    # If provided, a .weights.h5 file is loaded into the model
+    if path_to_weights is not None:
+        #Check compatibility
+        try:
+            model_e.load_weights(path_to_weights)
+        except(ValueError):
+            #If the file is not compatible with the shape of the model, 
+            # the model without any training is provided (as if 
+            # path_to_weights wasn't provided).
+            return model_e
     return model_e
 
 
-def DNN_xy(input_shape: Tuple=(7,3)) -> Model:
+def DNN_xy(input_shape: Tuple=(7,3), path_to_weights: str=None) -> Model:
     '''Definition of the Deep Neural Network model for hit coordinates [x,y] 
     reconstruction. This function defines the chosen architecture for the NN
     and returns the (untrained) keras.Model.
     Arguments
     ---------
-    - input_shape : Tuple
+    input_shape : Tuple
         Shape of the input in the for of a Tuple, that is the format taken as
         input by keras.Input(). The default value is set to (7,3), that is the
         shape of a photon track of 7 pixels, having for each pixel [pha, x, y].
+    path_to_weights : str
+        Path to a .weights.h5 file to be loaded on the model (after having checked
+        its compatibility with the NN architecture).
     '''
     model_xy = Sequential([
                     Input(shape=input_shape),
@@ -67,6 +83,16 @@ def DNN_xy(input_shape: Tuple=(7,3)) -> Model:
                     # Missing a Dropout layer, ask Rizzi []
                     Dense(2, activation='linear')
                     ])
+    # If provided, a .weights.h5 file is loaded into the model
+    if path_to_weights is not None:
+        #Check compatibility
+        try:
+            model_xy.load_weights(path_to_weights)
+        except(ValueError):
+            #If the file is not compatible with the shape of the model, 
+            # the model without any training is provided (as if 
+            # path_to_weights wasn't provided).
+            return model_xy
     return model_xy
 
 
